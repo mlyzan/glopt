c2f6f8693fb63b7d0710f98a08a5933d4b857d69
Volodymyr <mlyzan@ukr.net>
2019-09-04 14:11:08 +0300

    add mobile version

---
+++ dist/js/js.js
@@ -0,0 +1,31 @@
+ //modal
+ $('[data-modal=consultation]').on('click', ()=>{
+    $('.overlay, #consultation').fadeIn('slow');
+});
+
+$('.modal__close').on('click', ()=>{
+    $('.overlay, #consultation, #thanks').fadeOut('slow');
+});
+
+//form
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).send(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+    });
+
+//gamburger
+$('#gamburger').on('click', function(){
+    $('.nav').toggle('slow');
+    this.classList.toggle('gamburger_active');
+});
